4690b38eff93c0233f75830125fb2ce0fa94a8ae
Shravankumar623 <sharptechshravankumar@gmail.com>
2024-07-17 11:09:08 +0530

    Changes in App.js

--- Noteit/App.js
+++ Noteit/App.js
@@ -17,12 +17,5 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     backgroundColor: '#F9FBFC', // Set the background color to VERYLIGT BLUE
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  text: {
-    fontSize: 24, // Adjust the font size as needed
-    textAlign: 'center',
-    color: 'blue'
-  },
+  }
 })
